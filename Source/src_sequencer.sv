e61b97196c587d034dd95a8e9539ecc1ef9d0356
NicewinBenny <nicewinbenny@gmail.com>
2025-04-14 19:25:20 +0530

    Create src_sequencer.sv

---
+++ Source/src_sequencer.sv
@@ -0,0 +1,10 @@
+
+class src_sequencer extends uvm_sequencer #(src_trans);
+	`uvm_component_utils(src_sequencer)
+
+	extern function new(string name = "src_sequencer", uvm_component parent);
+endclass
+
+function src_sequencer::new(string name = "src_sequencer", uvm_component parent);
+	super.new(name, parent);
+endfunction
